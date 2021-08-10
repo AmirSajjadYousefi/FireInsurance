93ded8ad49de4ceb900e63f18777e4343033b5d4
amirsajjad yousefi <amirsj_yousefi@yahoo.com>
2021-08-10 15:16:41 +0430

    Finish Home UI

--- FireInsurance.Data/Models/Customer.cs
+++ FireInsurance.Data/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -8,11 +9,21 @@ namespace FireInsurance.Data.Models
     public partial class Customer
     {
         public string Id { get; set; }
+        [Required(ErrorMessage = "لطفا کد ملی را وارد نمایید")]
         public string NationalCode { get; set; }
+
+        [Required(ErrorMessage = "لطفا تاریخ تولد را وارد نمایید")]
         public string BirthDate { get; set; }
+
+        [Required(ErrorMessage = "لطفا کد پستی منزل را وارد نمایید")]
         public string PostalCode { get; set; }
+
+        [Required(ErrorMessage = "لطفا تلفن همراه را وارد نمایید")]
         public string Mobile { get; set; }
+
+        [Required(ErrorMessage = "لطفا کد محصول را وارد نمایید")]
         public int ProductId { get; set; }
+
         public DateTime CreationDateTime { get; set; }
     }
 }
